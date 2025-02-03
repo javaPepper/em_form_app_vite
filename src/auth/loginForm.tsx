87e07ef3473519f9588f009ff8c942c4dceb432f
Sergei Ananev <9852407799s@gmail.com>
2025-02-03 23:47:31 +0100

    feat: add formik

--- src/auth/loginForm.tsx
+++ src/auth/loginForm.tsx
@@ -1,17 +1,37 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { useFormik } from "formik";
 
 export function LoginForm() {
-
   const [isClosed, setClosed] = useState<boolean>(false);
   const [isChanged, setChanged] = useState<boolean>(false);
 
   const handleChangeVisibilty = () => {
     setChanged(!isChanged);
-  }
+  };
 
   const handleCloseForm = () => {
     setClosed(true);
-  }
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: (values, { setSubmitting }) => {
+      fetch('https://example.com')
+        .then((res: Response) => {
+          if(res.ok) {
+            console.log(values);
+          }
+            console.log(res);
+        })
+        .catch((err: Error) => {
+          alert(err.message);
+        })
+      setSubmitting(false);
+    },
+  });
 
   return (
     <div className='loginForm'>
@@ -19,46 +39,60 @@ export function LoginForm() {
         <div className='logoWrapper'></div>
         <div className='formArea'>
           <h1 className='header'></h1>
-          <form className='form'>
+          <form className='form' onSubmit={formik.handleSubmit}>
             <div className='textInputWrapper'>
-              <label htmlFor='inputName' className='inputLabel'>
+              <label htmlFor='email' className='inputLabel'>
                 Email
               </label>
               <input
                 type='email'
-                id='inputName'
-                className='textInput'
-                />
+                id='email'
+                className='email'
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                defaultValue={formik.values.email}
+              />
             </div>
             <div className='textInputWrapper'>
-              <label htmlFor='inputPassword' className='inputLabel'>
+              <label htmlFor='password' className='inputLabel'>
                 Password
               </label>
               <div className='passwordWrapper'>
                 <input
                   type={isChanged ? "text" : "password"}
-                  id='inputPassword'
-                  className='passwordInput'
+                  id='password'
+                  className='password'
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  defaultValue={formik.values.password}
                 />
                 <img
-                  src={isChanged ?
-                    "../src/assets/hide-password.svg" :
-                    "../src/assets/show-password.svg"}
+                  src={
+                    isChanged
+                      ? "../src/assets/hide-password.svg"
+                      : "../src/assets/show-password.svg"
+                  }
                   alt='showPassword'
                   className='passwordIcon'
                   onClick={handleChangeVisibilty}
                 />
               </div>
             </div>
-            <button className='button'>I'm in</button>
+            <button
+              className='button'
+              type='submit'
+              disabled={formik.isSubmitting}
+            >
+              I'm in
+            </button>
           </form>
         </div>
         <img
-          src="../src/assets/close-icon.svg"
+          src='../src/assets/close-icon.svg'
           alt='close'
           className='closeIcon'
           onClick={handleCloseForm}
-          />
+        />
       </div>
     </div>
   );
